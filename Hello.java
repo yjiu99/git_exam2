1b8c295a2158c3053f4ac794a12b3ae5871b033a
김예지 <yjiu9992@gmail.com>
2021-10-26 09:49:33 +0900

    201814095-5

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ package git_exam2;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("IT융합자율학부");
-        System.out.println("201814095");
-        System.out.println("김예지");
+        System.out.println("소속 : IT융합자율학부");
+        System.out.println("학번 : 201814095");
+        System.out.println("이름 : 김예지");
     }
 }
